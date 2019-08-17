6f18222e048a9679c502503bc0745b7475176b8e
kamauvick <kamauvick@outlook.com>
2019-08-17 19:21:31 +0300

    Add delete buttons on the quotes.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -6,21 +6,22 @@ import { Quote } from '../quote';
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote('Ninja', 'This is my ninja', 'vick', new Date(2019, 23, 3)),
     new Quote('Ninja two', 'This is my ninja', 'vick', new Date(2019, 23, 3)),
     new Quote('Ninja three', 'This is my ninja', 'vick', new Date(2019, 23, 3)),
-    new Quote('Ninja four', 'This is my ninja', 'vick', new Date(2019, 23, 3)),
-    new Quote('Ninja five', 'This is my ninja', 'vick', new Date(2019, 23, 3)),
-    new Quote('Ninja', 'This is my ninja', 'vick', new Date(2019, 23, 3)),
-    new Quote('Ninja', 'This is my ninja', 'vick', new Date(2019, 23, 3)),
-    new Quote('Ninja', 'This is my ninja', 'vick', new Date(2019, 23, 3)),
-    new Quote('Ninja', 'This is my ninja', 'vick', new Date(2019, 23, 3)),
-    new Quote('Ninja', 'This is my ninja', 'vick', new Date(2019, 23, 3)),
-    new Quote('Ninja', 'This is my ninja', 'vick', new Date(2019, 23, 3))
+    new Quote('Ninja four', 'This is my ninja', 'vick', new Date(2019, 23, 3))
   ];
 
+
+  addNewQuote(quote) {
+    const quoteLength = this.quotes.length;
+    quote.title = quoteLength + 1;
+    quote.date = new Date(quote.date);
+    this.quotes.push(quote);
+  }
 
   constructor() { }
 
